62300debde3b8a7d20b76aa21d770471089b55ef
princethakur19 <princethakur7778@gmail.com>
2026-04-17 17:36:54 +0530

    little update

--- practice.py
+++ practice.py
@@ -2,4 +2,4 @@ name = input("Enter your name here: ")
 age = int(input("Enter your age: "))
 
 print(f"Your name is {name}")
-print(f"You are {age} years old")
+print(f"You will be",  age + 5, "years old after 5 years")
