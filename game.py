83db5a11a10368c92cc020fe933f9fe074e34246
princethakur19 <princethakur7778@gmail.com>
2026-04-18 22:59:04 +0530

    Rock logic complete

--- game.py
+++ game.py
@@ -7,3 +7,13 @@ user_choice = input("Enter your move = Rock, Paper, Scissor: ")
 comp_choice = random.choice(item_list)
 
 print(f"User choice = {user_choice}, Computer choice = {comp_choice}")
+
+if user_choice == comp_choice:
+    print("Both chooses same: = Match Tie")
+
+elif user_choice == "Rock":
+    if comp_choice == "Paper":
+        print("Paper covers Rock = Computer")
+    else:
+        print("Rock smashes Scissor: You Win")
+
