c8aca49a1627d0efe99b32c66e91125c3e7eec57
princethakur19 <princethakur7778@gmail.com>
2026-04-18 22:49:16 +0530

    complete random choice to pick

--- game.py
+++ game.py
@@ -5,3 +5,5 @@ item_list = ["Rock", "Paper", "Scissor"]
 
 user_choice = input("Enter your move = Rock, Paper, Scissor: ")
 comp_choice = random.choice(item_list)
+
+print(f"User choice = {user_choice}, Computer choice = {comp_choice}")
