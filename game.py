73a646079df3e4c2ddcb2dcf40d1833cd352cbcb
princethakur19 <princethakur7778@gmail.com>
2026-04-18 22:44:04 +0530

    create variables

--- game.py
+++ game.py
@@ -1,3 +1,7 @@
 # Stone, Paper, Scissor
 
 import random
+item_list = ["Rock", "Paper", "Scissor"]
+
+user_choice = input("Enter your move = Rock, Paper, Scissor: ")
+comp_choice = random.choice(item_list)
