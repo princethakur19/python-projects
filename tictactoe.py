7a3d00a5f9e2e9bc8bc420b325f49ef02d3c7b47
princethakur19 <princethakur7778@gmail.com>
2026-04-20 19:18:42 +0530

    arrange button in grid

--- tictactoe.py
+++ tictactoe.py
@@ -24,4 +24,8 @@ def toggle_player():
 root = tk.Tk()
 root.title("Tic-Tac-Toe")
 
-buttons = [tk.Button(root, text="", font=("normal", 25), width=6, height=2, command=lambda)]
+buttons = [tk.Button(root, text="", font=("normal", 25), width=6, height=2, command=lambda i=i: button_click(i)) for i in range(9)]
+
+for i, button in enumerate(buttons):
+    buttons.grid(row=i //3, column=i % 3)
+
